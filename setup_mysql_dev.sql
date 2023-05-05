d45b5715a59badfadade062e1d6bb17622c29188
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-05-05 08:21:53 +0000

    SQL

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,10 +4,8 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 
 GRANT SELECT ON performance_scheme.* TO 'hbnb_dev'@'localhost';
-
 FLUSH PRIVILEGES;
